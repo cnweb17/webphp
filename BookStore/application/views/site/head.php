79bfd09a446153fc95bf4776470686bbe076b7cd
Luong <nguyenluong25797@gmail.com>
2018-05-10 12:03:56 +0700

    update js

--- BookStore/application/views/site/head.php
+++ BookStore/application/views/site/head.php
@@ -1,10 +1,10 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
 <title>The Book Store</title>
+<script src="<?php echo public_url('site'); ?>/js/jquery.min.js"></script>
+<script src="<?php echo public_url('site'); ?>/js/popper.min.js"></script>
+<script src="<?php echo public_url('site'); ?>/js/bootstrap.min.js"></script>
 <link href="<?php echo public_url('site'); ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
 <link href="<?php echo public_url('site'); ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 <link href="<?php echo public_url('site'); ?>/css/style.css" rel="stylesheet" type="text/css" />
 <link href="<?php echo public_url('site'); ?>/css/stylelogin.css" rel="stylesheet" type="text/css" />
